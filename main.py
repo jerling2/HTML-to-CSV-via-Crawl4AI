73504147646d1a1fddcccafde8a36b873e97864b
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2025-08-02 09:31:07 -0700

    Add abstract interface classes

--- main.py
+++ main.py
@@ -1,34 +1,15 @@
-from src.interface import FileProcessor, TableGenerator
-
-DATA_PATH = "./data"
-WEB_SCHEMA_PATH = "./web_schemas"
-JSON_OUT_PATH = "./json_out"
-TABLE_SCHEMA_PATH = "./table_schemas"
-TSV_OUT_PATH = "./tsv_out"
-
-
-def main():
-    GOODBYE = "Invalid input. Goodbye!"
-    mode = input("Hello! Please select [1] extract json from html or [2] construct csv from json: ")
-    try:
-        mode = int(mode)
-    except:
-        return print(GOODBYE)
-    
-    match mode:
-        case 1:
-            program = FileProcessor(WEB_SCHEMA_PATH, DATA_PATH, JSON_OUT_PATH)
-            program.interact()
-        case 2:
-            program = TableGenerator(TABLE_SCHEMA_PATH, JSON_OUT_PATH, TSV_OUT_PATH)
-            program.interact()
-        case _:
-            return print(GOODBYE)
-    return
+from src.interface import SystemMode
 
 
 if __name__ == "__main__":
+    main = SystemMode({
+        'schema_html': './configs/html_schemas',
+        'schema_table': './configs/table_schemas',
+        'data_content': './data/content',
+        'data_html': './data/html',
+        'data_table': './data/table'
+    })
     try:
-        main()
+        main.interact()
     except KeyboardInterrupt:
         print("\nGoodbye!")
