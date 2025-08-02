35378d6d805d2fb37d50a69b8ebf9fe7c21357e8
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2025-08-02 16:57:32 -0700

    Refactor caught up with main

--- src/interface/modes/json_to_tsv_mode.py
+++ src/interface/modes/json_to_tsv_mode.py
@@ -1,35 +1,19 @@
 import json
-import csv
-from src.interface import UserMode
+from ..abstracts.user_mode import UserMode
+from src.utils import generate_tsv
 
 
 class ConvertJsonToTSVMode(UserMode):
     
-    def _generate_tsv(self, schema, json_data, out_path):
-        selection = []
-        for data in json_data:
-            selection.extend(data.get(schema['selector'], []))            
-        headers = schema['headers']
-        rows = [headers]
-        for item in selection:
-            row = []
-            for header in headers:
-                filter_name = schema['filters'].get(header)
-                filter_fun = FILTER_MAP.get(filter_name, NOOP)
-                string = filter_fun(item.get(header, ""))
-                row.append(string)
-            rows.append(row)
-        with open(out_path, "w", newline="", encoding="utf-8") as f:
-            writer = csv.writer(f, delimiter="\t")
-            writer.writerows(rows)
-
-    def interact(self):
-        table_schema_path = self.prompt_choose('schema_table', "Please choose a schema")
+    def interact(self, table_schema_path=None, content_data_path=None):
+        if not table_schema_path:
+            table_schema_path = self.prompt_choose('schema_table', "Please choose a table schema")
         if not table_schema_path:
             return print("No table schema files.")
         with open(table_schema_path, 'r') as f:
             table_schema = json.load(f)
-        content_data_path = self.prompt_choose('data_content', "Please choose content data")
+        if not content_data_path:
+            content_data_path = self.prompt_choose('data_content', "Please choose content data")
         if not content_data_path:
             return print("No content data.")
         content_data_folder = self.get_basename(content_data_path)
@@ -43,62 +27,4 @@ class ConvertJsonToTSVMode(UserMode):
                 except Exception as e:
                     return print(f"Unexpected error: {e}")
         table_out_path = self.prompt_enter('data_table', "Please enter a name for the output file")
-        self._generate_tsv(table_schema, content, table_out_path)
-
-
-def NOOP(text: str) -> str:
-    return text
-
-
-def handshake_extend_href(text: str) -> str:
-    result = "https://uoregon.joinhandshake.com" + text
-    return result
-
-
-def handshake_extract_pay(text: str) -> str:
-    tokens = text.split('\u00b7')
-    filtered = list(filter(lambda t: "/" in t, tokens))
-    if not filtered:
-        return "N/A"
-    return filtered[0].strip()
-
-
-def handshake_extract_type(text: str) -> str:
-    tokens = text.split('\u00b7')
-    check = ["Part-time", "Full-time", "Internship"]
-    filtered = list(filter(lambda t: any(word in t for word in check), tokens))
-    if not filtered:
-        return "N/A"
-    return filtered[0].strip()
-
-
-def handshake_extract_duration(text: str) -> str:
-    tokens = text.split('\u00b7')
-    filtered = list(filter(lambda t: "\u2014" in t, tokens))
-    if not filtered:
-        return "N/A"
-    return filtered[0].strip()
-
-
-def handshake_extract_location(text: str) -> str:
-    tokens = text.split('\u00b7')
-    if not tokens:
-        return "N/A"
-    return tokens[0]
-
-
-def handshake_extract_deadline(text: str) -> str:
-    tokens = text.split('\u00b7')
-    if len(tokens) < 2:
-        return "N/A"
-    return tokens[1]
-
-
-FILTER_MAP = {
-    "handshake_extend_href": handshake_extend_href,
-    "handshake_extract_pay": handshake_extract_pay,
-    "handshake_extract_type": handshake_extract_type,
-    "handshake_extract_duration": handshake_extract_duration,
-    "handshake_extract_location": handshake_extract_location,
-    "handshake_extract_deadline": handshake_extract_deadline
-}
+        generate_tsv(table_schema, content, table_out_path)
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,1 +1,2 @@
 from .agent import extract_from_local_file
+from .file_manager import generate_tsv 
