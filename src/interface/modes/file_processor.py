c6ef67aed4898b969af463fa5eaeb07806ecf038
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2025-08-02 10:43:10 -0700

    Update file_processor

--- src/interface/modes/file_processor.py
+++ src/interface/modes/file_processor.py
@@ -1,66 +1,37 @@
 import asyncio
+import json
+from src.interface import UserMode
 from src.utils import extract_from_local_file
-from src.interface import Shell
 
 
-class FileProcessor(Shell):
-    def __init__(self, web_schema_path: str, data_path: str, json_out_path: str):
-        super().__init__(
-            web_schema_path=web_schema_path, 
-            json_out_path=json_out_path, 
-            data_path=data_path
-        )
-
-    def _prompt_web_schema(self) -> dict:
-        available_schemas = self._ls(self.web_schema_path, "*.json")
-        if not available_schemas:
-            print("Uh oh, no available schemas")
-            return
-        user_input = input(f"Please choose a schema {available_schemas}: ")
-        while user_input not in available_schemas:
-            print("Invalid selection")
-            user_input = input(f"Please choose a schema {available_schemas}: ")
-        path = self._get_web_schema_file_path(user_input)
-        try:
-            return self._load_json(path)
-        except Exception as e:
-            print(f"Json Load Error: {e}")
-            return None
-
-    def _prompt_data(self) -> [str]:
-        available_folders = self._ls_dir(self.data_path)
-        user_input = input(f"Please choose a folder {available_folders}: ")
-        while user_input not in available_folders:
-            print("Invalid selection")
-            user_input = input(f"Please choose a folder {available_folders}: ")
-        path = self._get_data_folder_path(user_input)
-        files = self._ls_file_uri(path)
-        return files
-
-    def _prompt_json_out(self) -> str:
-        user_input = input(f"Please enter a name for the output folder: ")
-        path = self._get_json_out_folder_path(user_input)
-        while self._is_path_taken(path):
-            if input("Name is taken. Overwrite [y]? ") == "y":
-                self._rm_rf(path)
-                break
-            user_input = input(f"Please enter a name for the output folder: ")
-            path = self._get_json_out_folder_path(user_input)
-        self._mkdir(path)
-        return user_input
-
+class LocalExtractMode(UserMode):
+    
     def interact(self):
-        schema = self._prompt_web_schema()
-        if not schema:
-            return
-        URIs = self._prompt_data()
-        out_folder = self._prompt_json_out()
+        html_schema_path = self.prompt_choose('schema_html', "Please choose a schema")
+        if not html_schema_path:
+            return print("No available html schemas.")
+        with open(html_schema_path, 'r') as f:
+            schema = json.load(f)
+        html_data = self.prompt_choose('data_html', "Please choose html data")
+        if not html_data:
+            return print("No available html data.")
+        folder = self.get_basename(html_data)
+        files = self.ls('data_html', dir_name=folder, include_extention=False)
+        paths = ['file://' + self.get_path('data_html', dir_name=folder, file_name=file) for file in files]
+        content_out_path = self.prompt_enter('data_content', "Please enter a name for the output folder")
+        content_folder = self.get_basename(content_out_path)
         page_num = 0
-        for uri in URIs:
-            json_data = asyncio.run(extract_from_local_file(uri, schema))
-            if not json_data:
+        for path in paths:
+            result = asyncio.run(extract_from_local_file(path, schema))
+            if not result:
                 continue
-            out_path = self._get_json_out_file_path(out_folder, f'page {page_num}')
-            self._dump_json_to_file(out_path, json_data)
+            out_path = self.get_path('data_content', file_name=f'page_{page_num}', dir_name=content_folder)
+            with open(out_path, 'w') as f:
+                json.dump(result, f, indent=2)
             page_num += 1
-        return
+        
+        # num = 0
+        # for each file path,
+        #   asyncio.run  ( agent )
+        #   output json to path with basename = path_{num}.json
+        #   inc num
