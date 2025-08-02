c51b6b7f595f68deb95dcf00eb6b4c19a2d81978
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2025-08-02 13:21:36 -0700

    Remove artifact code

--- src/interface/__init__.py
+++ src/interface/__init__.py
@@ -1,5 +1,5 @@
 from .abstracts.shell import Shell
 from .abstracts.user_mode import UserMode
-from .modes.file_processor import LocalExtractMode
-from .modes.table_generator import ConvertJsonToCSVMode
+from .modes.local_extract_mode import LocalExtractMode
+from .modes.json_to_tsv_mode import ConvertJsonToTSVMode
 from .modes.system_mode import SystemMode
